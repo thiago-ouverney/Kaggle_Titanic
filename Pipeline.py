235a34bbc9ed6797f9214e8edefb50f66e4089d6
81018590 <thiago.ouverney@vale.com>
2021-07-26 17:23:33 -0300

    function get_model trained

--- Pipeline.py
+++ Pipeline.py
@@ -25,6 +25,7 @@ def drop_columns_with_null_valuse(df):
 def drop_categorical_features(df):
     df = df.select_dtypes(exclude="object")
     return df
+
 def saving_columns(df):
     global colunas
     colunas= df.columns
@@ -38,35 +39,6 @@ def sex_to_binary(n):
     elif n == 'female':
         return 0
 
-#transform Pclass int stype to categorical through get_dummies
-def Pclass_onehot(df):
-    df_Pclass_enc = pd.get_dummies(df['Pclass'])
-    return df_Pclass_enc
-
-# transform  all columns necessary
-def transform_dtype(df):
-    df['Gender_binary'] = df['Sex'].map(sex_to_binary)
-    Pclass_dummies = Pclass_onehot(df)
-    df = df.join(Pclass_dummies)
-    df.drop(['Name' ,'Ticket' ,'Cabin' ,'Embarked' ,'Sex' ,'Pclass'], axis=1, inplace=True)
-    try:
-        df["Fare"] = df["Fare"].fillna(0)
-    except:
-        pass
-    return df
-
-# dealing with missing numbers
-def dealing_null_values(df):
-    df['Age'] = df['Age'].fillna(-1)
-    return df
-
-def saving_columns(df):
-    global colunas
-    colunas= df.columns
-    return df
-
-
-#-----------------------------------------
 
 def drop_columns_with_null_valuse(df):
     df.dropna(axis=1, inplace=True)
@@ -82,21 +54,31 @@ def drop_categorical_features(df):
     return df
 
 
-def saving_columns(df):
-    global colunas
-    colunas = df.columns
+def one_hot_encoder(df,columns):
+    for column in columns:
+        df_aux = pd.get_dummies(df[column],prefix=column)
+        df = df.join(df_aux)
+        df.drop(column,axis=1,inplace=True)
     return df
 
+# dealing with missing numbers
+def dealing_null_values(df):
+    df['Age'] = df['Age'].fillna(-1)
+    return df
+
+# transform  all columns necessary
+def transform_dtype_new(df,teste="ok"):
+    df = one_hot_encoder(df, ["Sex","Pclass","Embarked"])
+    df.drop(['Name' ,'Ticket' ,'Cabin'], axis=1, inplace=True)
+    if teste != None:
+        df["Fare"] = df["Fare"].fillna(0)
+
+    return df
 
 get_drop_columns_with_null_valuse = FunctionTransformer(drop_columns_with_null_valuse, validate=False)
 get_drop_categorical_features = FunctionTransformer(drop_categorical_features, validate=False)
 
 get_dealing_null_values = FunctionTransformer(dealing_null_values,validate=False)
-get_transform_dtype = FunctionTransformer(transform_dtype,validate=False)
-get_colums_names = FunctionTransformer(saving_columns,validate=False)
-
-get_drop_columns_with_null_valuse = FunctionTransformer(drop_columns_with_null_valuse,validate=False)
-get_drop_categorical_features = FunctionTransformer(drop_categorical_features,validate=False)
-get_colums_names = FunctionTransformer(saving_columns,validate=False)
+get_transform_dtype_new = FunctionTransformer(transform_dtype_new,validate=False)
 
 
--- train.py
+++ train.py
@@ -1,15 +1,12 @@
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
-from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
+from sklearn.model_selection import train_test_split,  cross_val_score, KFold, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-
 import warnings
 warnings.filterwarnings("ignore")
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 from Pipeline import *
 
 
@@ -22,80 +19,94 @@ seed = 0
 
 pipe_RF = Pipeline(memory=None,
                  steps = [
-                     ("Feature_Selection:get_transform_dtype",get_transform_dtype),
+                     ("Dtype_Columns",get_transform_dtype_new),
                      ("Null_Validate:get_dealing_null_values",get_dealing_null_values),
-                     ("Final_Columns:get_colums_names",get_colums_names),
                      ("RandomForest", RandomForestClassifier(random_state=seed) )
                  ],
                 verbose=False
                 )
 
 RF_params = {
+    'Dtype_Columns__kw_args': [ {'teste': "ok"} ],
     'RandomForest__criterion': ["gini"],
     'RandomForest__bootstrap': [True,False],
     'RandomForest__min_samples_leaf': [3,5, 10],
     'RandomForest__max_depth': [15,20, 25,27, 30],
     'RandomForest__n_estimators': [200, 500,700]
 }
-
-RF_best_params = {'RandomForest__bootstrap': [False],
+RF_best_params = {
+ 'Dtype_Columns__kw_args': [ {'teste': "ok"} ],  #Aqui conseguimos colocar usando __kw_args, argumentos proprios
+  'RandomForest__bootstrap': [False],
  'RandomForest__criterion': ['gini'],
  'RandomForest__max_depth': [15],
  'RandomForest__min_samples_leaf': [3],
  'RandomForest__n_estimators': [200]}
 
-pipe_LR = Pipeline(memory=None,
-                 steps = [
-                     ("Feature_Selection:get_transform_dtype",get_transform_dtype),
-                     ("Null_Validate:get_dealing_null_values",get_dealing_null_values),
-                     ("Final_Columns:get_colums_names",get_colums_names),
-                     ("ModelLR", LogisticRegression(random_state=seed) )
-                 ],
-                verbose=False
-                )
-
-
-
 #Salvando Scores
-modelos_testados = {"Modelos":["RandomForestClassifier"],
-                    "Pipeline":[pipe_RF],
-                    "Score":[],
+modelos_testados = {"Modelos":[],
+                    "Score_Validation":[],
+                    "Score_Test":[],
                     "Steps":[],
                     "Params":[]
                     }
 
 
-with open("metrics.txt", 'w') as outfile:
-    pipe = modelos_testados["Pipeline"][0]
-    clf = GridSearchCV(pipe, RF_best_params,verbose=3,cv=5)
-    clf.fit(x, y)
-    test_score = clf.best_score_
-    steps = clf.estimator.named_steps.keys()
-    params = clf.best_params_
-    nome_modelo = "RandomForestClassifier"
-    outfile.write(f"{nome_modelo}- Test Score: {test_score} - Steps: {steps}")
+# Predição para Baseline
+def saving_predict(X,y,folds,seed,pipe,nome_modelo,grid_params = ""):
 
-    modelos_testados["Score"].append(test_score)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(1 / folds), random_state=seed)
+
+    if grid_params:
+        pipe_analise = GridSearchCV(pipe, grid_params, verbose=1,cv=folds)
+        pipe_analise.fit(X_train, y_train)
+        steps = pipe_analise.estimator.named_steps.keys()
+        validation_score = pipe_analise.best_score_
+        params = pipe_analise.best_params_
+        modelos_testados["Params"].append(params)
+
+    else:
+        pipe_analise = pipe
+        pipe_analise.fit(X_train,y_train)
+        cv = KFold(n_splits=(folds))
+        scores = cross_val_score(pipe, X_train, y_train, cv=cv)
+        validation_score = sum(scores)/len(scores)
+
+        modelos_testados["Params"].append("To Do")
+        steps = pipe.named_steps.keys()
+
+
+    #Saving test metric
+    test_score = pipe_analise.score(X_test,y_test)
     lista_steps = [step for step in steps]
+
+    modelos_testados["Modelos"].append(nome_modelo)
     modelos_testados["Steps"].append(lista_steps)
+    modelos_testados["Score_Validation"].append(validation_score)
+    modelos_testados["Score_Test"].append(test_score)
+    return pipe_analise
+
+# Teste
+x_test = pd.read_csv("Dados/test.csv",index_col = 0)
 
 
+# Saving my predictions
+pipe_1 = saving_predict(x,y,folds= 5, seed = seed, pipe = pipe_RF,grid_params = RF_best_params, nome_modelo= "RF Parametros Otimizados")
+#saving_predict(x,y,folds= 5, seed = seed, pipe = pipe_RF,grid_params = RF_params, nome_modelo= "RF Parametros a se otimizar")
+pipe_2 = saving_predict(x,y,folds= 5, seed = seed, pipe = pipe_RF, nome_modelo= "RF Cross Validation Baseline")
 
 
-
-df_modelos = pd.DataFrame({"Model":modelos_testados["Modelos"], "Score":modelos_testados["Score"], "Steps":modelos_testados["Steps"]})
+#Saving Final Dataframe
+df_modelos = pd.DataFrame(modelos_testados)
 df_modelos.to_markdown("Modelos.md",index=False)
 
 
 #Salvando submissão com Max Score dentre nossos pipelines
-x_test = pd.read_csv("Dados/test.csv",index_col = 0)
-predict_array = clf.predict(x_test)
-predict_submission = pd.DataFrame({"PassengerId":x_test.index,"Survived":predict_array})
 
-predict_submission.to_csv("Predições/Predict2.csv",index=False)
+#predict_array = clf.predict(x_test)
+#predict_submission = pd.DataFrame({"PassengerId":x_test.index,"Survived":predict_array})
 
-print(df_modelos.Score)
-print("FINALIZADO!!!")
+#predict_submission.to_csv("Predições/Predict2.csv",index=False)
+
 
 
 
